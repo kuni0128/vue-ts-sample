6bc937d57e5100ff7e5bf76ce7fbc52e1c4ab91e
Kuniaki Ishikawa <k.ishikawa.0128@gmail.com>
2020-02-29 16:18:01 +0900

    modify ResetButton initial value when created

--- src/components/ResetButton.vue
+++ src/components/ResetButton.vue
@@ -8,13 +8,16 @@ import { Component, Emit, Prop, Vue } from 'vue-property-decorator';
 @Component
 export default class ResetButton extends Vue {
   @Prop()
-  public initialValue!: string;
-
-  @Prop()
   public value!: string;
+
+  private initialValue!: string;
 
   @Emit()
   public input(value: string) {}
+
+  public created() {
+    this.initialValue = this.value;
+  }
 
   public onClick() {
       this.input(this.initialValue);
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
       <MyButton :greet="greetText" @click="onMyButtonClicked"></MyButton>
     </p>
     <p>
-      <ResetButton initialValue="Hello" v-model="greetText"></ResetButton>
+      <ResetButton v-model="greetText"></ResetButton>
     </p>
   </div>
 </template>
