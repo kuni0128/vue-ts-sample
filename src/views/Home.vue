1fa10a759f12cbe80c162d01c8e684e6ec2df7dc
Kuniaki Ishikawa <k.ishikawa.0128@gmail.com>
2020-02-29 16:48:02 +0900

    add message with computed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <p>{{ greetText }}</p>
     <p>挨拶した回数 : {{ count }}</p>
+    <p v-if="isRegulars">いつもありがとうございます</p>
     <p>
       <MyButton :greet="greetText" @click="onMyButtonClicked"></MyButton>
     </p>
@@ -25,6 +26,10 @@ import ResetButton from '@/components/ResetButton.vue';
 export default class Home extends Vue {
   public greetText: string = 'Hello';
   private count: number = 0;
+
+  public get isRegulars(): boolean {
+    return this.count >= 3;
+  }
 
   public onMyButtonClicked(count: number) {
     this.count = count;
